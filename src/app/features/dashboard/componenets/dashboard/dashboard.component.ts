1ae959b533c69d54d22f9d037bcd057de910f128
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2025-01-23 13:37:36 +0000

    Add header and footer

--- src/app/features/dashboard/componenets/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/componenets/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../../shared/header/header.component';
+import { FooterComponent } from '../../../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LogoCourtierComponent } from "./logo-courtier/logo-courtier.component";
+import { LogoProfileCourtierComponent } from "./logo-profile-courtier/logo-profile-courtier.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [LogoCourtierComponent, LogoProfileCourtierComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
